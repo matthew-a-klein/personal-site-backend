9c0f2faa52d8ff5806d898dfa18cde4a8490b622
Matthew Klein <matthew-a-klein@github.com>
2023-10-29 10:12:21 +0000

    fix debug

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG") == "1"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
 
